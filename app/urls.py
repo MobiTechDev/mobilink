7ca58820600543f2e572bfde4459ee74c710acc2
M.H <mike.hersh.work@gmail.com>
2023-05-08 15:56:07 +0300

    On Development

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import HomeView
 from . import views
 
-app_name='app'
+app_name = 'app'
 
 urlpatterns = [
-    path('',HomeView.as_view(),name='home'),
-    path('list/',views.list,name='list')
+    path('', HomeView.as_view(), name='home'),
+    path('list/', views.list_ports, name='list')
 ]
--- app/views.py
+++ app/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView,FormView,CreateView,ListView,DetailView,UpdateView,DeleteView
-from .import models
+from django.views.generic import TemplateView, FormView, CreateView, ListView, DetailView, UpdateView, DeleteView
+from . import models
 from serial.tools.list_ports import comports
+
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'app/home.html'
 
-def list(request):
+
+def list_ports(request):
     ports = []
-    for n, (port, desc, hwid) in enumerate(sorted(comports()), 1):
-        #sys.stderr.write('{:2}: {:20} {!r}\n'.format(n, port, desc))
+    for n, (port, desc, hid) in enumerate(sorted(comports()), 1):
+        # sys.stderr.write('{:2}: {:20} {!r}\n'.format(n, port, desc))
         if 'NULL' not in port:
             ports.append(port)
-    
-    context = {'all_ports':ports}
-    return render(request,'app/list.html',context=context)
+
+    context = {'all_ports': ports}
+    return render(request, 'app/list.html', context=context)
